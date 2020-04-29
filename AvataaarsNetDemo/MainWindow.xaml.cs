884b694f0cd91540cb6998ab1e1177a5a7a8354e
informagico <alessandro.magoga92@gmail.com>
2020-04-29 17:57:02 +0200

    Initial commit

---
+++ AvataaarsNetDemo/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using AvataaarsNet.Models;
+using Newtonsoft.Json;
+
+namespace AvataaarNetDemo
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		private Bitmap _Avatar;
+		public Bitmap Avatar
+		{
+			get
+			{
+				return _Avatar;
+			}
+			set
+			{
+				if (_Avatar == value)
+					return;
+
+				_Avatar = value;
+
+				OnPropertyChanged();
+			}
+		}
+
+		private AvataaarsConfiguration _Configuration;
+		public AvataaarsConfiguration Configuration
+		{
+			get
+			{
+				return _Configuration;
+			}
+			set
+			{
+				if (_Configuration == value)
+					return;
+
+				_Configuration = value;
+
+				OnPropertyChanged();
+			}
+		}
+
+		private bool _EnableWidth = true;
+		public bool EnableWidth
+		{
+			get
+			{
+				return _EnableWidth;
+			}
+			set
+			{
+				if (_EnableWidth == value)
+					return;
+
+				_EnableWidth = value;
+
+				OnPropertyChanged();
+			}
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = this;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+		{
+			PropertyChangedEventHandler handler = this.PropertyChanged;
+			if (handler != null)
+			{
+				var e = new PropertyChangedEventArgs(propertyName);
+				handler(this, e);
+			}
+		}
+
+		private void SaveFile_Click(object sender, RoutedEventArgs e)
+		{
+			Avatar.Save("avataaars.png", ImageFormat.Png);
+		}
+
+		private void SaveConfig_Click(object sender, RoutedEventArgs e)
+		{
+			string json = JsonConvert.SerializeObject(Configuration);
+			File.WriteAllText("configuration.json", json);
+		}
+
+		private void LoadConfig_Click(object sender, RoutedEventArgs e)
+		{
+			string json = File.ReadAllText("configuration.json");
+			Configuration = JsonConvert.DeserializeObject<AvataaarsConfiguration>(json);
+		}
+
+		private void RandomizeConfig_Click(object sender, RoutedEventArgs e)
+		{
+			Configuration = AvataaarsSettings.Randomize(Configuration.AvatarWidth);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# avataaaarsnet-demo
+# AvataaarsNet - DEMO
+
+This is a working demo for the [AvataaarsNet WPF Controller](https://github.com/informagico/avataaarsnet)
+
+![demo](./assets/demo.png)
+
+## Compile
+
+To properly run the demo you can refer to the [Releases](https://github.com/informagico/avataaarsnet-demo/releases) tab (that version uses https://avataaars.io/ endpoint) or you can compile it yourself by adding a reference to this project: [AvataaarsNet](https://github.com/informagico/avataaarsnet).
+
+In any case you can also compile the .dll file from the linked repository (adjusting the endpoint if needed) and then adding that to the resources.
+
+## Note
+
+In this demo I use [MaterialDesignInXAML](https://github.com/MaterialDesignInXAML) for a better UI
